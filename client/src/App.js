eb53d5673e33339dfefe10b63f4a2a9b8cb14233
bayl0n <nathanbaylon@gmail.com>
2021-05-02 03:33:58 +1000

    Created button and adjusted sass structure

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import NavItem from './components/NavItem';
 import NavFill from './components/NavFill';
 import NavLogo from './components/NavLogo';
 
+import IntroImg from './icons/illustration-intro.png'
+
 function App() {
   return (
     <div>
@@ -23,8 +25,13 @@ function App() {
         </NavItem>
       </Navbar>
 
-      <main>
-        Hello!
+      <main className="intro-main">
+        <section className="intro-img-container">
+          <img src={IntroImg} className="intro-img" />
+          <h1>Track all your habits so you can shit the bed.</h1>
+          <p>We understand that sometimes shitting the bed is very annoying, so what we have done at HabitTracker is track when you need to take a massive dookie.</p>
+          <button className="secondary-btn">Get Started</button>
+        </section>
       </main>
     </div>
   );
